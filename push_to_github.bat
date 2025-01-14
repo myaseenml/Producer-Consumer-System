050b86d9e1752759add05cf22ff40780e0374d02
Muhammad Yaseen <66284136+myaseenml@users.noreply.github.com>
2025-01-14 15:15:18 +0500

    Delete push_to_github.bat

--- push_to_github.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git init
-git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/myaseenml/Producer-Consumer-System.git
-git branch -M main
-git push -u origin main
-pause
